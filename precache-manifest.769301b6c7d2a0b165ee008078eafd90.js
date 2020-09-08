709ab2f67df04909098e5d126d920c3fd92f7c36
Gabriel Monteiro do Nascimento <gabriel.monteiro00@gmail.com>
2020-09-07 22:57:31 -0300

    Add files - trying to publish on github pages

---
+++ precache-manifest.769301b6c7d2a0b165ee008078eafd90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6733c7929138fff9e5f97ddb45747d65",
+    "url": "/openweatherreactapp/index.html"
+  },
+  {
+    "revision": "6a269c90ba893e70c3b6",
+    "url": "/openweatherreactapp/static/css/main.166973b9.chunk.css"
+  },
+  {
+    "revision": "3186a43636484d3609a7",
+    "url": "/openweatherreactapp/static/js/2.a78314b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/openweatherreactapp/static/js/2.a78314b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a269c90ba893e70c3b6",
+    "url": "/openweatherreactapp/static/js/main.26c13c58.chunk.js"
+  },
+  {
+    "revision": "81ed6dacf1b3aa4f8ed8",
+    "url": "/openweatherreactapp/static/js/runtime-main.194cd79b.js"
+  },
+  {
+    "revision": "86a2115b453997c578353e3e3bda3f1e",
+    "url": "/openweatherreactapp/static/media/logo.86a2115b.png"
+  }
+]);
